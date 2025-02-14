a456fcc4ad46e09001389c93d21c0242fd1fa3e7
abelzavaleta <abel@origin.63>
2025-02-14 02:28:39 -0400

    Backend: hash password

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 // Define the interface for the User model
 export interface IUser extends Document {
@@ -19,6 +20,20 @@ const UserSchema: Schema<IUser> = new Schema({
         required: true
     }
 }, { timestamps: true });
+
+// Hash the password before saving the user
+UserSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error: any) {
+        next(error);
+    }
+});
 
 // Create the User model
 export const UserModel: Model<IUser> = mongoose.model<IUser>('User', UserSchema);
