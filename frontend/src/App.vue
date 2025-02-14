a9e54d6b37793d20c8e01c3944d492818317fce7
abelzavaleta <abel@origin.63>
2025-02-14 07:37:46 -0400

    Frontned: change icon on home view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Ridery logo" class="logo" src="@/assets/ridery-icon.jpg" width="300" height="300" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
