8afa64b5acdafbeb1855fc7c6a749279cd4d7257
abelzavaleta <abel@origin.63>
2025-02-14 09:51:43 -0400

    Frontend: implement guard auth on routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,20 +2,41 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AdminView from '../views/AdminView.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView,
+    meta: { requiresAuth: true },
+  }
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: AdminView,
-    },
-  ],
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Ruta protegida
+    if (!token) {
+      next({ name: '' });
+    } else {
+      next();
+    }
+  } else if ((to.name === 'home') && token) {
+    // Redirigir a admin si el usuario ya está autenticado
+    next({ name: 'admin' });
+  } else {
+    next();
+  }
+});
+
+export default router;
